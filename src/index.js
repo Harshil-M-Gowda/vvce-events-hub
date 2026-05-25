edcbed3037ad9a79a3aea7cafb82ac3a32315500
Harshil M Gowda <vvce25cse0239@vvce.ac.in>
2026-05-25 23:18:58 +0530

    Fixed Render proxy issue

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const { errorHandler, notFound } = require('./middleware/errorHandler');
 const { pool } = require('./config/database');
 
 const app = express();
+app.set('trust proxy', 1)
 const PORT = process.env.PORT || 5000;
 
 // ─── Ensure uploads directory exists ─────────────────────────────────────────
